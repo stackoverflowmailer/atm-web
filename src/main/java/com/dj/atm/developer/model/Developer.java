eed2c338ad26a0aaa538d1d887e0a683c20a7ae2
deepakjacob@gmail.com <deepakjacob@gmail.com@71b05469-a1fb-631d-b848-30d4254393cd>
2010-05-28 13:39:35 +0000

    Misc. modifications.

--- src/main/java/com/dj/atm/developer/model/Developer.java
+++ src/main/java/com/dj/atm/developer/model/Developer.java
@@ -10,9 +10,10 @@ import java.util.Date;
 public class Developer extends Entity {
 
     private static final long serialVersionUID = 1L;
+    private String title;
     private Name name;
     private Phone landPhone;
-    // private Phone mobilePhone;
+    private Phone mobilePhone;
     private Band band;
     /**
      * Date of joining the project *
@@ -24,86 +25,67 @@ public class Developer extends Entity {
     private Date dol;
     private String bloodGroup;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Name getName() {
-	return name;
+        return name;
     }
 
     public void setName(Name name) {
-	this.name = name;
+        this.name = name;
     }
 
     public Phone getLandPhone() {
-	return landPhone;
+        return landPhone;
     }
 
     public void setLandPhone(Phone landPhone) {
-	this.landPhone = landPhone;
+        this.landPhone = landPhone;
     }
 
-    /*
-     * public Phone getMobilePhone() { return mobilePhone; }
-     * 
-     * public void setMobilePhone(Phone mobilePhone) { this.mobilePhone =
-     * mobilePhone; }
-     */
+    public Phone getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public void setMobilePhone(Phone mobilePhone) {
+        this.mobilePhone = mobilePhone;
+    }
 
     public Band getBand() {
-	return band;
+        return band;
     }
 
     public void setBand(Band band) {
-	this.band = band;
+        this.band = band;
     }
 
     public Date getDoj() {
-	return doj;
+        return doj;
     }
 
     public void setDoj(Date doj) {
-	this.doj = doj;
+        this.doj = doj;
     }
 
     public Date getDol() {
-	return dol;
+        return dol;
     }
 
     public void setDol(Date dol) {
-	this.dol = dol;
+        this.dol = dol;
     }
 
     public String getBloodGroup() {
-	return bloodGroup;
+        return bloodGroup;
     }
 
     public void setBloodGroup(String bloodGroup) {
-	this.bloodGroup = bloodGroup;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Developer developer = (Developer) o;
-
-        if (band != developer.band) return false;
-        if (bloodGroup != null ? !bloodGroup.equals(developer.bloodGroup) : developer.bloodGroup != null) return false;
-        if (doj != null ? !doj.equals(developer.doj) : developer.doj != null) return false;
-        if (dol != null ? !dol.equals(developer.dol) : developer.dol != null) return false;
-        if (landPhone != null ? !landPhone.equals(developer.landPhone) : developer.landPhone != null) return false;
-        if (name != null ? !name.equals(developer.name) : developer.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + (landPhone != null ? landPhone.hashCode() : 0);
-        result = 31 * result + (band != null ? band.hashCode() : 0);
-        result = 31 * result + (doj != null ? doj.hashCode() : 0);
-        result = 31 * result + (dol != null ? dol.hashCode() : 0);
-        result = 31 * result + (bloodGroup != null ? bloodGroup.hashCode() : 0);
-        return result;
+        this.bloodGroup = bloodGroup;
     }
 }
